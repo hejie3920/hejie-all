d7e5bc536ea031ab547d8435377afae6f9bb9495
hejie <1484215077@qq.com>
2020-05-28 12:19:23 +0800

    uni-app添加云函数

---
+++ hejie-app+统一多端/uni-app/study/main.js
@@ -0,0 +1,14 @@
+import Vue from "vue"
+import App from "./App"
+// import element from "element-ui"
+// import "element-ui/lib/theme-chalk/index.css"
+
+Vue.config.productionTip = false
+// Vue.use(element)
+
+App.mpType = "app"
+
+const app = new Vue({
+  ...App
+})
+app.$mount()
---
+++ hejie-app+统一多端/uni-app/study/pages/index/index.vue
@@ -0,0 +1,65 @@
+<template>
+	<view class="bg-content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">	
+			<text class="title test">{{title}}asdfk</text>
+		</view>
+		<view>
+			<button type="default">你好</button>
+			<label>
+				<checkbox value="askdlfj" /><text>asdfasfd</text>
+				<checkbox value="askdlfj" /><text>asdf</text>
+			</label>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.bg-content {
+		.test{
+			color: red;
+		}
+	}
+	
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
