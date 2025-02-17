63fb18f2f1eea8d4c91657497f6bd44bbf6d219d
zhenghejie <zhanghejie@corp.netease.com>
2025-02-18 05:51:14 +0800

    feat: 补充

--- 个人部分博客文档总结和脚本/node.js
+++ 个人部分博客文档总结和脚本/node.js
@@ -1,29 +1,45 @@
-async function async1() {
-  console.log("async1 start");
-  await async2();
-  console.log("async1 end");
+async function fn1(next) {
+  console.log("fn1");
+  await next();
+  console.log("end fn1");
 }
-async function async2() {
-  console.log("async2");
+async function fn2(next) {
+  console.log("fn2");
+  await delay();
+  await next();
+  console.log("end fn2");
 }
-console.log("script start");
-setTimeout(function () {
-  console.log("setTimeout0");
-}, 0);
-setTimeout(function () {
-  console.log("setTimeout3");
-}, 3);
-setImmediate(() => console.log("setImmediate"));
-process.nextTick(() => console.log("nextTick"));
-async1();
-new Promise(function (resolve) {
-  console.log("promise1");
-  resolve();
-  console.log("promise2");
-}).then(function () {
-  console.log("promise3");
-});
-console.log("script end");
+function fn3(next) {
+  console.log("fn3");
+}
+function delay() {
+  return new Promise((reslove, reject) => {
+      setTimeout(() => {
+          reslove();
+      }, 2000);
+  });
+}
+const middlewares = [fn1, fn2, fn3];
+const finalFn = compose(middlewares);
+finalFn();
+// 杰koacompose
+function compose(middlewares) {
+  return function () {
+      return dispatch(0); // 执行第0个
+      function dispatch(i) {
+          let fn = middlewares[i];
+          if (!fn) {
+              return Promise.resolve();
+          }
+          return Promise.resolve(
+              fn(function next() {
+                  // promise完成后，再执行下一个
+                  return dispatch(i + 1);
+              })
+          );
+      }
+  };
+}
 // function deepCopy(obj) {
 //     if (typeof obj !== "object" || obj === null) {
 //         return obj;
